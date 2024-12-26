444927f4537ac8c13cd7818b2f532a88c267fcef
luter <newgglutt@gmail.com>
2024-12-26 17:02:32 +0200

    dev

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,29 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const parseUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    const parts = parseUrl.pathname.split('/').filter((part) => part !== '');
+    const query = {};
+
+    parseUrl.searchParams.forEach((value, key) => {
+      query[key] = value;
+    });
+
+    const response = {
+      parts,
+      query,
+    };
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(response));
+  });
+
+  return server;
 }
 
 module.exports = {
